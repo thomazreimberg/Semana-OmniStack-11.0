a961a74264d00b501e6ed465c4c8f9550b723667
Thomaz Henrique <treimberg7@gmail.com>
2020-03-29 02:27:43 -0300

    Creating project

---
+++ aulas/backend/src/routes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const OngController = require('./constrollers/OngController');
+const IncidentsController = require('./constrollers/IncidentController');
+const ProfileController = require('./constrollers/ProfileController');
+const SessionController = require('./constrollers/SessionController');
+
+const routes = express.Router();
+
+routes.post('/sessions', SessionController.create);
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+routes.get('/profile', ProfileController.index);
+
+routes.get('/incidents', IncidentsController.index);
+routes.post('/incidents', IncidentsController.create);
+routes.delete('/incidents/:id', IncidentsController.delete);
+
+module.exports = routes;
